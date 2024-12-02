8810baa7610938680a225baa339a60ba0b08643d
SabaShagidzeMain <sabadoeswebdev@gmail.com>
2024-12-02 23:49:16 +0400

    feat: Static Main Page, Dummy API (Posts/Reviews)

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,9 +1,21 @@
+import React from 'react';
+import PostList from './Components/PostList/PostList';
+import ReviewList from './Components/ReviewList/ReviewList';
 import styles from './page.module.css';
 
-export default function Home() {
+const MainPage: React.FC = () => {
   return (
-    <div className={styles.main}>
-      <h1>Main Page</h1>
-    </div>
+    <>
+      <div className={styles.main_wrapper}>
+        <div className={styles.post_wrapper}>
+          <PostList />
+        </div>
+        <div className={styles.review_wrapper}>
+          <ReviewList />
+        </div>
+      </div>
+    </>
   );
-}
+};
+
+export default MainPage;
