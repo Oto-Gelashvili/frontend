07c590378b6029471a941d3bead387a4b1a5acd8
Shota Jokhadze <jokhadzeshota8@gmail.com>
2024-12-04 12:33:43 +0400

    feat(auth): add Create New Password page UI

--- front/app/Components/Buttons.tsx
+++ front/app/Components/Buttons.tsx
@@ -3,13 +3,13 @@ import React from "react";
 interface ButtonProps {
   className?: string;
   content: string;
-  onClick?: () => void;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement> | React.FormEvent<HTMLFormElement>) => void;
   type?: "button" | "submit" | "reset";
+  disabled?: boolean;
 }
-
-export default function Button({ className, content, onClick, type = "button" }: ButtonProps) {
+export default function Button({ className, content, onClick, type = "button", disabled = false }: ButtonProps) {
   return (
-    <button type={type} className={className} onClick={onClick}>
+    <button type={type} className={className} onClick={onClick}  disabled={disabled}>
       {content}
     </button>
   );
